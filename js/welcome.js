4e7dda4ff02b92b40b2f5d4ba1c89a1f91ef29f9
Kimberly Garcés <kimberlygarces1994@gmail.com>
2021-09-09 16:21:19 -0500

    version1.2.5(pagina principal)

---
+++ js/welcome.js
@@ -0,0 +1,7 @@
+const welcome = document.getElementById('welcome'); //Obtiene todos elementos del HTML con la clase .mySlide
+
+
+function hiddenWelcome(){
+    welcome.style.display = "none"; // Oculta los slides posición por posición
+    // welcome[0].style.zIndex = "1"; // Oculta los slides posición por posición
+}
